19701105468eb9dc68b5cab2b0cb7ed7d1d5498e
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-05-30 15:27:57 -0300

    Modelo de 'usuário' criado.

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,1 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin
+    )
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name='Email de usuário',
+        max_length=194,
+        unique=True,
+        )
+    is_active = models.BooleanField(
+        verbose_name='Usuário está ativo',
+        default=True,
+        )
+    is_staff = models.BooleanField(
+        verbose_name="Usuário é da equipe de desenvolvimento",
+        default=False,
+    )
+    is_superuser = models.BooleanField(
+        verbose_name="Usuário é um Super Usuário",
+        default=False,
+    )
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        db_table = 'usuario'
+
+    def __str__(self):
+        return self.email
