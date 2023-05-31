02842b54477907ae7ba0524789fa2171328dc531
Nicolas Galdino <nicolasesmael1998@gmail.com>
2023-05-31 15:02:20 -0300

    Formulário de registro de visitantes criado e mensagem de feedback adicionada..

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
 from visitantes.forms import VisitanteForm
+from django.shortcuts import render, redirect
 
 
 def registrar_visitante(request):
     form = VisitanteForm()
+
+    if request.method == 'POST':
+        form = VisitanteForm(request.POST)
+
+        if form.is_valid():
+            visitante = form.save(commit=False)
+
+            visitante.registrado_por = request.user.porteiro
+            visitante.save()
+
+            messages.success(
+                request,
+                'Visitante registrado com sucesso!'
+            )
+
+            return redirect('index')
+
     context = {
         'nome_pagina': 'Registrar Visitante',
         'form': form,
